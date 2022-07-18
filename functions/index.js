70f3c94d96827071c8227852874247062ec12f04
Raul C. Rivero <rahulrcr98@gmail.com>
2022-07-18 09:29:23 -0400

    add headers to response

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,9 @@ const getUpcomingMatches = require("../utils/getUpcomingMatches")
 
 const headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
 }
 
 exports.handler = async function () {
