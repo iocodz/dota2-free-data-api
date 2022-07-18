fb2ff188790f4f0e8895862a7dbd5d61e8bcd962
Raul C. Rivero <rahulrcr98@gmail.com>
2022-07-18 09:09:53 -0400

    fix cors headers

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,25 @@
 const getUpcomingMatches = require("../utils/getUpcomingMatches")
 
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+}
+
 exports.handler = async function () {
     try {
         const data = await getUpcomingMatches()
         return {
-            headers: {
-                "Access-Control-Allow-Origin": "*"
-            },
             statusCode: 200,
+            headers,
             body: JSON.stringify({
                 data
             })
         }
     } catch (e) {
         return {
-            headers: {
-                "Access-Control-Allow-Origin": "*"
-            },
             statusCode: 500,
+            headers,
             body: JSON.stringify({
                 e
             })
